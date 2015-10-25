140f1fb8a0f1c8a70234b34990dde769757b65af
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-10-25 14:50:29 +0900

    Added show, update and create actions in user controller, and created show.html.haml file.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,21 @@
 class UserController < ApplicationController
+
   def index
   end
+
   def new
   end
+
   def edit
   end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
 end
