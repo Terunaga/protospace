a1de5d7a681ae5662bee8a41edd99e327e7bce16
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-10-31 15:36:38 +0900

    Added contents to update action such that modifications to profile will be updated.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,9 +13,16 @@ class UserController < ApplicationController
   end
 
   def update
+    current_user.update(update_params)
+    redirect_to controller: :prototypes, action: :index
   end
 
   def create
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:username, :email, :member, :profile, :works)
   end
 
 end
