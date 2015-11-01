2e0cc02cb9bf9e0982a1ed5fee1b39eb53503087
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-11-02 01:24:40 +0900

    Changed the "concept" column type from string to text.

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 20151101145036) do
   create_table "prototypes", force: :cascade do |t|
     t.string   "title",      limit: 255
     t.string   "catch_copy", limit: 255
-    t.string   "concept",    limit: 255
+    t.text     "concept",    limit: 65535
     t.integer  "user_id",    limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade do |t|
