47f2a37b547f08bb893c8bb3e73e78f1c5c7a65e
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-11-02 01:24:05 +0900

    Added contents to new action and create action in prototypes controller.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -7,6 +7,17 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @prototype = Prototype.new
   end
 
+  def create
+    Prototype.create(create_params)
+    redirect_to :root and return
+  end
+
+  private
+  def create_params
+    params.require(:prototype).require(:prototypes).permit(:title, :catch_copy, :concept).merge(user_id: current_user.id)
+  end
 end
+
