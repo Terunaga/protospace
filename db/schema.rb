61210474752df7dce821cdcd7b0d6b1a6635fd7a
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-11-02 14:29:27 +0900

    Migrations got messed up, so I deleted the prototype tables, ran rails d model prototype, and recreated the model with the necessary columns.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101145036) do
+ActiveRecord::Schema.define(version: 20151102052138) do
 
   create_table "prototypes", force: :cascade do |t|
     t.string   "title",      limit: 255
