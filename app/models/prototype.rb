a0beeb4db36586e395bf9d6277ff5d34d3f75a2e
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-11-04 19:24:15 +0900

    Defined some instance methods which will call main and sub thumbnails from thumbnails table.

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -6,6 +6,19 @@ class Prototype < ActiveRecord::Base
   accepts_nested_attributes_for :thumbnails
 
   validates_presence_of :title, :catch_copy, :concept
+
+
+  def main_thumbnail
+    thumbnails.main.first.name
+  end
+
+  def first_thumbnail
+    thumbnails.sub.first.name?
+  end
+
+  def second_thumbnail
+    thumbnails.sub.second.name?
+  end
 end
 
 
