136568c0a6499ca7f761f0120381caf4e87b8512
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-11-09 01:36:32 +0900

    Created "common_code" method to simplify edit, update and destroy actions.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
 
+  before_action :common_code, only: [:edit, :update, :destroy]
+
   def index
     @prototypes = Prototype.includes(:thumbnails)
   end
@@ -23,28 +25,30 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
+    binding.pry
     @prototype.update(update_params)
     redirect_to :root
   end
 
   def destroy
-    prototype = Prototype.find(params[:id])
-    prototype.destroy
+    @prototype.destroy
     redirect_to :root
   end
 
   private
   def create_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, thumbnails_attributes: [:name, :status, :prototype_id])
+    params.require(:prototype).permit(:title, :catch_copy, :concept, thumbnails_attributes: [:name, :status])
   end
 
   def update_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, thumbnails_attributes: [:name, :status, :prototype_id])
+    params.require(:prototype).permit(:title, :catch_copy, :concept, thumbnails_attributes: [:name, :status])
+  end
+
+  def common_code
+    @prototype = Prototype.find(params[:id])
   end
 end
 
