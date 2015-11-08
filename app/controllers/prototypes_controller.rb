5d99ef40b3a42e7c9133f5cd227c6c4e668fd8be
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-11-08 23:57:55 +0900

    Added contents to edit, update and destroy action in prototypes controller.

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,8 +22,28 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(update_params)
+    redirect_to :root
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to :root
+  end
+
   private
   def create_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, thumbnails_attributes: [:name, :status, :prototype_id])
+  end
+
+  def update_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, thumbnails_attributes: [:name, :status, :prototype_id])
   end
 end
