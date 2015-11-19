8a6e49ec92043052edf4ce45927453d5da4a0af3
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-11-19 16:35:47 +0900

    Created instance variables for いいね feature.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,17 @@
 class LikesController < ApplicationController
   def create
-    prototype = Prototype.find(params[:prototype_id])
-    like = current_user.likes.create(prototype_id: prototype.id)
+    @prototype = Prototype.find(params[:prototype_id])
+    @like = Like.where(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @likes = Like.where(prototype_id: params[:prototype_id])
+    like = current_user.likes.create(prototype_id: params[:prototype_id])
     like.save
-    redirect_to controller: 'prototypes', action: 'show', id: params[:prototype_id]
   end
 
   def destroy
     like = Like.find(params[:id])
     like.destroy
-    redirect_to controller: 'prototypes', action: 'show', id: params[:prototype_id]
+    @prototype = Prototype.find(params[:prototype_id])
+    @like = Like.where(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @likes = Like.where(prototype_id: params[:prototype_id])
   end
 end
