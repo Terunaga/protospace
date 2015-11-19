10b9f64a98d9e28a1f6e7b5c0ba30f60e87ea5d5
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-11-19 11:51:14 +0900

    Made some modifications that were necessary as a result of the nesting.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
   def create
-    prototype = Prototype.find(params[:format])
-    like = current_user.likes.build(prototype_id: prototype.id)
+    prototype = Prototype.find(params[:prototype_id])
+    like = current_user.likes.create(prototype_id: prototype.id)
     like.save
-    redirect_to controller: 'prototypes', action: 'show', id: params[:format]
+    redirect_to controller: 'prototypes', action: 'show', id: params[:prototype_id]
   end
 
   def destroy
     like = Like.find(params[:id])
     like.destroy
-    redirect_to :root
+    redirect_to controller: 'prototypes', action: 'show', id: params[:prototype_id]
   end
 end
