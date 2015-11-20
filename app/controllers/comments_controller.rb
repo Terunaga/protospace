c5d31946674fb44866b755086a3f7e80a4b36653
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-11-21 01:56:24 +0900

    Added necessary instance variables to controllers.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,7 @@
 class CommentsController < ApplicationController
   def create
     comment = current_user.comments.create(create_params)
-    # @comment = Comment.new
     @comments = Comment.where(prototype_id: params[:prototype_id]).order('created_at ASC')
-    # redirect_to controller: 'prototypes', action: 'show', id: params[:prototype_id]
   end
 
   private
