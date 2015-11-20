a41a4edd7aef137f69d5e5b8a215da40bdf70bcc
Terunaga Tamura <terunaga_tamura@hotmail.co.jp>
2015-11-20 23:11:01 +0900

    Modified, and simplified the likes controller.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,20 @@
 class LikesController < ApplicationController
+
   def create
-    @prototype = Prototype.find(params[:prototype_id])
-    @like = Like.where(user_id: current_user.id, prototype_id: params[:prototype_id])
-    @likes = Like.where(prototype_id: params[:prototype_id])
     like = current_user.likes.create(prototype_id: params[:prototype_id])
-    like.save
+    likes_set
   end
 
   def destroy
     like = Like.find(params[:id])
     like.destroy
+    likes_set
+  end
+
+  private
+  def likes_set
     @prototype = Prototype.find(params[:prototype_id])
-    @like = Like.where(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
   end
 end
